d6f0af0a2fd01e03735b6d806a52c7fdb73759df
RDeveloper123 <vasimakramc@gmail.com>
2024-04-12 20:35:07 +0530

    App updated

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import './App.css';
 import Home from './component/Home';
 import Project from './component/Project';
 import Contact from './component/Contact';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <>
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/Home' element={<Home/>}></Route>
         <Route path='/Project' element={<Project/>}></Route>
         <Route path='/Contact' element={<Contact/>}></Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
      
     </>
   );
